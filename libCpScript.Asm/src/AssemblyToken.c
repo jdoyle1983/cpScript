a05421fc913bd169be3c4e0d2333f94cae63f4cd
jdoyle1983 <jdoyle1983@gmail.com>
2012-07-12 10:07:37 -0400

    Fixed CodeBlocks project configuration issues. Removed files that are not used.

--- libCpScript.Asm/src/AssemblyToken.c
+++ libCpScript.Asm/src/AssemblyToken.c
@@ -1,4 +1,5 @@
 #include <AssemblyToken.h>
+#include <Token.h>
 #include <string.h>
 #include <malloc.h>
 
@@ -7,7 +8,10 @@ AssemblyToken* AssemblyToken_New(short Tok, const char* Val)
 	AssemblyToken* t = (AssemblyToken*)malloc(sizeof(AssemblyToken));
 	t->Tok = Tok;
 	t->Val = NULL;
-	if(Val != NULL)
+	short shouldAllocString = 0;
+	if(Tok == tRegister || Tok == tMemoryVar || Tok == tLiteral || Tok == tQuotedLiteral)
+        shouldAllocString = 1;
+	if(Val != NULL && shouldAllocString == 1)
 	{
 		t->Val = (char*)malloc(sizeof(char*) * (strlen(Val) + 1));
 		strcpy(t->Val, Val);
