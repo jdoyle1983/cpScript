c22a44df1b2babe2b0f200e0522254067f3277b7
Jason Doyle <jdoyle1983@gmail.com>
2016-08-23 19:49:51 -0400

    Updates for new runtime.

--- NewRuntimeProto/NewRuntimeProto/Program.cs
+++ NewRuntimeProto/NewRuntimeProto/Program.cs
@@ -6,8 +6,29 @@ namespace NewRuntimeProto
 {
     class Program
     {
+        static string TestProgram = @"\
+; Nothing Here
+JMP _Start
+
+_TestLabel:
+    POP @1
+    MOV &2, @2
+    JMP @1
+
+_Start:
+    PUSH @1
+    PUSH \""Your Stuff\""
+    PUSH 'More Stuff'
+    PUSHR
+    PUSH %CLOC + 2
+    JMP _TestLabel
+    MOV %Data, @2
+    POPR
+";
         static void Main(string[] args)
         {
+            State tState = new State();
+            tState.Load(TestProgram);
         }
     }
 }
--- NewRuntimeProto/NewRuntimeProto/State.cs
+++ NewRuntimeProto/NewRuntimeProto/State.cs
@@ -6,26 +6,158 @@ namespace NewRuntimeProto
 {
     public class State
     {
-        public string[] Registers = { "", "", "", "", "", "", "", "", "", "", "", "" };
-        public string[] Memory;
-        public List<string> Stack;
+        internal static char[] SplitChars = {
+                                                ' ',
+                                                '%',
+                                                '@',
+                                                '&',
+                                                '[',
+                                                ']',
+                                                ',',
+                                                '+',
+                                                '-'
+                                            };
+        internal string[] Script;
+        internal List<List<string>> ScriptPart;
+        internal string[] Register = { "", "", "", "", "", "", "", "", "", "", "", "" };
+        internal string[] Memory;
+        internal List<string> Stack;
+        internal Dictionary<string, int> LabelOffsets;
+        internal int CurrentScriptOffset = -1;
 
         public State()
         {
+            Script = new string[0];
+            ScriptPart = new List<List<string>>();
             Memory = new string[0];
             Stack = new List<string>();
+            LabelOffsets = new Dictionary<string, int>();
+        }
+
+        public void Load(string AsmText)
+        {
+            //Split script by line breaks
+            string[] SrcLines = AsmText.Split(new char[] { '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            //Find genuine code lines, removing comments (lines beginning with ;) and the like.
+            List<string> CleanLines = new List<string>();
+            foreach (string s in SrcLines)
+                if (s.Trim() != "" && !s.Trim().StartsWith(";"))
+                    CleanLines.Add(s.Trim());
+
+            //Store entire clean script
+            Script = CleanLines.ToArray();
+
+            //Pre-Split all lines
+            foreach (string s in Script)
+            {
+                List<string> tItems = new List<string>(s.SplitAndKeep(State.SplitChars));
+                List<string> fItems = new List<string>();
+                char QuoteChar = ' ';
+                string QuoteString = "";
+                foreach (string p in tItems)
+                {
+                    if(QuoteChar == ' ')
+                    {
+                        if(p.StartsWith("\""))
+                        {
+                            QuoteChar = '\"';
+                            QuoteString += p;
+                        }
+                        else if(p.StartsWith("'"))
+                        {
+                            QuoteChar = '\'';
+                            QuoteString += p;
+                        }
+                        else
+                        {
+                            if (p.Trim() != "")
+                                fItems.Add(p.Trim());
+                        }
+                    }
+                    else
+                    {
+                        QuoteString += p;
+                        if(p.EndsWith(QuoteChar.ToString()))
+                        {
+                            QuoteChar = ' ';
+                            fItems.Add(QuoteString);
+                        }
+                    }
+                }
+                ScriptPart.Add(fItems);
+            }
+
+            //Find each label offset
+            //Example:
+            //MyLabel:
+            for (int i = 0; i < Script.Length; i++)
+                if (Script[i].EndsWith(":"))
+                    LabelOffsets.Add(Script[i].Replace(":", ""), i);
+        }
+
+        public void Reset()
+        {
+            Register[0] = Register[1] = Register[2] = Register[3] = Register[4] = Register[5] = Register[6] = Register[7] = Register[8] = Register[9] = Register[10] = Register[11] = "";
+            Memory = new string[0];
+            CurrentScriptOffset = -1;
+            Stack.Clear();
+        }
+
+        public bool Iterate()
+        {
+            CurrentScriptOffset++;
+
+            if(CurrentScriptOffset >= Script.Length)
+            {
+                CurrentScriptOffset = Script.Length - 1;
+                return false;
+            }
+
+            if (ScriptPart[CurrentScriptOffset][0].EndsWith(":"))
+                CurrentScriptOffset++;
+
+            if (CurrentScriptOffset >= Script.Length)
+            {
+                CurrentScriptOffset = Script.Length - 1;
+                return false;
+            }
+
+            switch(ScriptPart[CurrentScriptOffset][0].ToLower())
+            {
+                case "push":
+                    {
+
+                    } break;
+                case "pop":
+                    {
+
+                    } break;
+                case "pushr":
+                    {
+                        for (int i = 0; i < 12; i++)
+                            StackPush(Register[i]);
+                    } break;
+                case "popr":
+                    {
+                        for (int i = 11; i >= 0; i--)
+                            Register[i] = StackPop();
+                    } break;
+            }
+
+            return true;
         }
 
         #region Memory Management
 
-        internal string ReadMemoryOffset(long Offset)
+        public string ReadMemoryOffset(long Offset)
         {
             if (Offset < 0 || Offset >= Memory.LongLength)
                 throw new Exception("Read Outside Memory Allocated.");
             return Memory[Offset];
         }
 
-        internal void WriteMemoryOffset(long Offset, string Value)
+        public void WriteMemoryOffset(long Offset, string Value)
         {
             if (Offset < 0)
                 throw new Exception("Write Outside Bounds.");
@@ -52,11 +184,11 @@ namespace NewRuntimeProto
 
         #region Register Management
 
-        internal string ReadRegister(short RegisterIndex)
+        public string ReadRegister(short RegisterIndex)
         {
             if (RegisterIndex < 0 || RegisterIndex >= 12)
                 throw new Exception("Invalid Register");
-            return Registers[RegisterIndex];
+            return Register[RegisterIndex];
         }
 
         internal string ReadRegister(string RegisterText)
@@ -77,11 +209,11 @@ namespace NewRuntimeProto
             }
         }
 
-        internal void SetRegister(short RegisterIndex, string Value)
+        public void SetRegister(short RegisterIndex, string Value)
         {
             if (RegisterIndex < 0 || RegisterIndex >= 12)
                 throw new Exception("Invalid Register");
-            Registers[RegisterIndex] = Value;
+            Register[RegisterIndex] = Value;
         }
 
         internal void WriteRegister(string RegisterText, string Value)
@@ -106,19 +238,19 @@ namespace NewRuntimeProto
 
         #region Stack Management
 
-        internal void StackPush(string Value)
+        public void StackPush(string Value)
         {
             Stack.Add(Value);
         }
 
-        internal string StackPeek()
+        public string StackPeek()
         {
             if (Stack.Count < 1)
                 throw new Exception("Stack Exception, Peek / Pop Empty Stack.");
             return Stack[Stack.Count - 1];
         }
 
-        internal string StackPop()
+        public string StackPop()
         {
             string Val = StackPeek();
             Stack.RemoveAt(Stack.Count - 1);
