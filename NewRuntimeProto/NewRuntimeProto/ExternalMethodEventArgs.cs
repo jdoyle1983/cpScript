62ea95d99be42cee41ce220de5334676ad6a1b90
Jason Doyle <jdoyle@capture-education.com>
2016-08-25 12:00:25 -0400

    Added some missing project files.

---
+++ NewRuntimeProto/NewRuntimeProto/ExternalMethodEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NewRuntimeProto
+{
+    public delegate void ExternalMethodHandler(ExternalMethodEventArgs e);
+
+    public class ExternalMethodEventArgs
+    {
+        public string ExternalMethodRequested { get; set; }
+        public State ScriptState { get; set; }
+
+        public ExternalMethodEventArgs(string externalMethodRequested, State scriptState)
+        {
+            ExternalMethodRequested = externalMethodRequested;
+            ScriptState = scriptState;
+        }
+    }
+}
---
+++ NewRuntimeProto/NewRuntimeProto/MemoryBlock.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NewRuntimeProto
+{
+    public class MemoryBlock
+    {
+        public bool Used { get; set; }
+        public string Value { get; set; }
+
+        public MemoryBlock()
+        {
+            Used = false;
+            Value = "";
+        }
+
+        public MemoryBlock(string initialValue)
+        {
+            Used = true;
+            Value = initialValue;
+        }
+
+        public void ReAlloc(string newValue)
+        {
+            Used = true;
+            Value = newValue;
+        }
+
+        public void Free()
+        {
+            Used = false;
+            Value = "";
+        }
+    }
+}
